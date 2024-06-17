ab6c56fdd8c1e13752b649c7def52782b251215b
Pollux <polluxprinters@protonmail.com>
2024-06-17 11:08:59 -0400

    Add in javascript to aid with image rescaling on page reload

--- script.js
+++ script.js
@@ -2,18 +2,75 @@ function scaleImages() {
 	const containers = document.querySelectorAll('.offer-img-container');
 	containers.forEach(container => {
 		const img = container.querySelector('.offer-img');
-		if (img) {
-			const imgNaturalWidth = img.naturalWidth;
-			const imgNaturalHeight = img.naturalHeight;
+		if (img.complete) {
+			// Image already loaded
+			const imgWidth = img.offsetWidth;
+			const imgHeight = img.offsetHeight;
+			console.log(`Image displayed with dimensions: ${imgWidth}x${imgHeight}`);
+			
 			const containerWidth = container.clientWidth;
 			const containerHeight = container.clientHeight;
-
-			const widthRatio = containerWidth / imgNaturalWidth;
-			const heightRatio = containerHeight / imgNaturalHeight;
-			const scale = Math.max(widthRatio, heightRatio);
-
-			img.style.width = `${imgNaturalWidth * scale}px`;
-			img.style.height = `${imgNaturalHeight * scale}px`;
+			console.log(`Container has with dimensions: ${containerWidth}x${containerHeight}`);
+			
+			const widthRatio = imgWidth / containerWidth;
+			const heightRatio = imgHeight / containerHeight;
+			console.log(`Ratios: ${widthRatio}x${heightRatio}`);
+			
+			if (widthRatio <= heightRatio) //Scale horizontally
+			{
+				console.log("horizontal");
+				scaledWidth = imgWidth / widthRatio;
+				scaledHeight = imgHeight / widthRatio;
+				img.style.width = `${scaledWidth}px`;
+				img.style.height = `${scaledHeight}px`;
+				console.log("New Height: " + img.style.height);
+				console.log("New Width: " + img.style.width);
+			}
+			else //Scake vertically
+			{
+				console.log("vertical");
+				scaledWidth = imgWidth / heightRatio;
+				scaledHeight = imgHeight / heightRatio;
+				img.style.width = `${scaledWidth}px`;
+				img.style.height = `${scaledHeight}px`;
+				console.log("New Height: " + img.style.height);
+				console.log("New Width: " + img.style.width);
+			}
+		} else {
+			img.addEventListener('load', function() {
+				const imgWidth = img.offsetWidth;
+				const imgHeight = img.offsetHeight;
+				console.log(`Image displayed with dimensions: ${imgWidth}x${imgHeight}`);
+				
+				const containerWidth = container.clientWidth;
+				const containerHeight = container.clientHeight;
+				console.log(`Container has with dimensions: ${containerWidth}x${containerHeight}`);
+				
+				const widthRatio = imgWidth / containerWidth;
+				const heightRatio = imgHeight / containerHeight;
+				console.log(`Ratios: ${widthRatio}x${heightRatio}`);
+				
+				if (widthRatio <= heightRatio) //Scale horizontally
+				{
+					console.log("horizontal");
+					scaledWidth = imgWidth / widthRatio;
+					scaledHeight = imgHeight / widthRatio;
+					img.style.width = `${scaledWidth}px`;
+					img.style.height = `${scaledHeight}px`;
+					console.log("New Height: " + img.style.height);
+					console.log("New Width: " + img.style.width);
+				}
+				else //Scake vertically
+				{
+					console.log("vertical");
+					scaledWidth = imgWidth / heightRatio;
+					scaledHeight = imgHeight / heightRatio;
+					img.style.width = `${scaledWidth}px`;
+					img.style.height = `${scaledHeight}px`;
+					console.log("New Height: " + img.style.height);
+					console.log("New Width: " + img.style.width);
+				}
+			});
 		}
 	});
 }
